1becbfbbaf3fd0f4f250b0fb50f9e5e77eac0bb5
csgoat0 <elturkabdelrahman@gmain.com>
2026-02-09 14:20:59 +0200

    Adding Service Extensions

---
+++ TheCharityBLL/Services/ServiceExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TheCharityDAL.Database;
+using TheCharityDAL.Repositories.Abstraction;
+using TheCharityDAL.Repositories.Implementation;
+
+namespace TheCharityBLL.Services
+{
+    public static class ServiceExtensions
+    {
+        public static void TheCharityEnhancedConnectionString(this IServiceCollection services, IConfiguration configuration, string stringName = "defaultConnection")
+        {
+            var connectionString = configuration.GetConnectionString(stringName);
+            services.AddDbContext<TheCharityDbContext>(options =>
+                options.UseSqlServer(connectionString));
+        }
+        public static void TheCharityDependencyInjection(this IServiceCollection services)
+        {
+            services.AddScoped<ICampaignRepository, CampaignRepository>();
+            services.AddScoped<IDonatedItemsRepository, DonatedItemsRepository>();
+            services.AddScoped<IDonationRepository, DonationRepository>();
+            services.AddScoped<IOrganizationRepository, OrganizationRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+        }
+        public static void ThirdPartyAuthentication(this IServiceCollection services)
+        {
+            services.AddAuthentication()
+                .AddGoogle(options =>
+                {
+                    // Configure Google authentication options here
+                    options.ClientId = "";
+                    options.ClientSecret = "";
+                })
+                .AddFacebook(options =>
+                {
+                    // Configure FaceBook authentication options here
+                    options.AppId = "";
+                    options.AppSecret = "";
+                });
+        }
+    }
+}
--- TheCharityPL/Program.cs
+++ TheCharityPL/Program.cs
@@ -1,4 +1,5 @@
-
+using TheCharityBLL.Helpers;
+using TheCharityBLL.Services;
 namespace TheCharityPL
 {
     public class Program
@@ -7,8 +8,9 @@ namespace TheCharityPL
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.TheCharityEnhancedConnectionString(builder.Configuration);
+            builder.Services.TheCharityDependencyInjection();
             // Add services to the container.
-
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
